d0ea53344f5bda97314401ed7da03159fe0d6028
josh <abcmattttt@gmial.com>
2023-10-18 13:54:13 +0800

    test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import WebApp from '@twa-dev/sdk';
 
 WebApp.ready();
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
