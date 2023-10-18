6537021bee6aa5bc139a5472fd707a925d2ad60a
josh <abcmattttt@gmial.com>
2023-10-18 11:20:17 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
   <div>
     <RouterLink to="/">Home</RouterLink>
@@ -10,5 +6,25 @@ import { RouterLink, RouterView } from 'vue-router'
 
   <RouterView />
 </template>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import WebApp from '@twa-dev/sdk';
 
+WebApp.ready();
+WebApp.enableClosingConfirmation();
+let updateHeightFlag = true;
+let height = Math.ceil(WebApp.viewportHeight);
+WebApp.onEvent('viewportChanged', ({ isStateStable }) => {
+  const currentHeight = Math.ceil(WebApp.viewportHeight);
+
+  if (height > currentHeight && !isStateStable) {
+    WebApp.expand();
+  }
+
+  if (updateHeightFlag && WebApp.isExpanded) {
+    height = currentHeight;
+    updateHeightFlag = false;
+  }
+});
+</script>
 <style scoped></style>
